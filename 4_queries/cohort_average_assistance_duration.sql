5bab3c841dddd6559392cbff9ffe0f7c2c31ca5d
Prashant R <smplsoln@gmail.com>
2021-10-12 17:50:14 -0600

    Add teachers, assistances, cohorts sqls

---
+++ 4_queries/cohort_average_assistance_duration.sql
@@ -0,0 +1,14 @@
+SELECT AVG(total_duration) AS average_total_duration
+FROM (
+  SELECT cohorts.name AS cohort,
+    SUM(completed_at - started_at)
+      AS total_duration
+  FROM assistance_requests
+  JOIN students
+    ON students.id = assistance_requests.student_id
+  JOIN cohorts
+    ON cohorts.id = students.cohort_id
+  GROUP BY cohorts.name
+  ORDER BY total_duration
+) AS cohorts_total_durations
+;
---
+++ 4_queries/total_cohort_assistance_duration.sql
@@ -0,0 +1,11 @@
+SELECT cohorts.name AS cohort,
+  SUM(completed_at - started_at)
+    AS total_duration
+FROM assistance_requests
+JOIN students
+  ON students.id = assistance_requests.student_id
+JOIN cohorts
+  ON cohorts.id = students.cohort_id
+GROUP BY cohorts.name
+ORDER BY total_duration ASC
+;
