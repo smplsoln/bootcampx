db4f43f787ababc2ae73e85d3187b6fdaa1e78ec
Prashant R <smplsoln@gmail.com>
2021-10-12 18:53:18 -0600

    Add students assistance requests count sqls

---
+++ 4_queries/students_assistance_requests_count.sql
@@ -0,0 +1,10 @@
+SELECT COUNT(assistance_requests.student_id) AS total_assistances,
+        students.name AS name
+FROM assistance_requests
+JOIN students
+  ON assistance_requests.student_id = students.id
+WHERE students.name = 'Elliot Dickinson'
+GROUP BY students.name
+
+;
+
